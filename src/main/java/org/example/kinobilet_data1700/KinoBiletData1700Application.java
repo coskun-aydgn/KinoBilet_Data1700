d727a6805af64cec4372ba798852493e23242633
coskunaydgn <coskun.aydgn@gmail.com>
2024-04-15 23:21:46 +0200

    Initial commit

---
+++ src/main/java/org/example/kinobilet_data1700/KinoBiletData1700Application.java
@@ -0,0 +1,13 @@
+package org.example.kinobilet_data1700;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoBiletData1700Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinoBiletData1700Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/kinobilet_data1700/KinoBiletData1700ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kinobilet_data1700;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinoBiletData1700ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
